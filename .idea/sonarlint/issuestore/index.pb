217c7a72b356e9e784dc1224da0401c992160000
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2021-09-17 14:20:04 +0530

    Chunking

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -27,9 +27,6 @@ _
 /src/test/kotlin/cci/recursion/MagicIndexTest.kt,f/4/f4f0e0a4ad09f1b12c918b6ddef2551493de9aab
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-:
-
-.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
 =
 .editorconfig,6/2/626a747c8694eaae66f2afb3d1a745b045782c70
 Q
@@ -94,3 +91,5 @@ A
 gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
 ]
 -src/main/kotlin/leetcode/greedy/CarPooling.kt,c/2/c20c2aafda23003ae9c0dab5801d07589821328c
+`
+0src/main/kotlin/leetcode/graph/CourseSchedule.kt,8/3/83f38e3b970d1a4bdb60b39a102fc29acaac5386
--- src/main/kotlin/leetcode/greedy/Chunking.kt
+++ src/main/kotlin/leetcode/greedy/Chunking.kt
@@ -2,32 +2,39 @@ package leetcode.greedy
 
 fun chunking(records: Map<Int, Int>, batchSize: Int): Int {
   val sameChunkIds = mutableListOf<Int>()
-  val batchesItr = records.entries.filter { it.value > 200 }.chunked(batchSize).map { batch -> batch.sortedBy { it.value } }.iterator()
+  val batchesItr =
+    records.entries.filter { it.value > 200 }.chunked(batchSize).map { batch -> batch.sortedBy { it.value } }.iterator()
+
   var chunks = 0
   var accumulatedPendingTaxLineCount = 0
   while (true) {
     if (!batchesItr.hasNext()) {
       if (sameChunkIds.isNotEmpty()) {
         chunks++
+        // Update
       }
       return chunks
     }
     for (batch in batchesItr.next()) {
       val currentCount = batch.value
       accumulatedPendingTaxLineCount += currentCount
+      
       if (accumulatedPendingTaxLineCount <= 500) {
         sameChunkIds.add(batch.key)
       } else {
         if (sameChunkIds.isNotEmpty()) {
           chunks++
+          // Update
+          sameChunkIds.clear()
         }
+        // New accumulation
         accumulatedPendingTaxLineCount = currentCount
-        sameChunkIds.clear()
         sameChunkIds.add(batch.key)
       }
     }
     if (accumulatedPendingTaxLineCount > 500) {
-      chunks++
+      chunks++ 
+      // Update
       sameChunkIds.clear()
       accumulatedPendingTaxLineCount = 0
     }
