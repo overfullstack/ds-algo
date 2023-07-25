cee728ad2498c1ead246631b8bd4ae97488ee87a
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2023-07-25 20:05:11 +0530

    ProductSum

---
+++ ds-algo/src/main/kotlin/algoexpert/recursion/ProductSum.kt
@@ -0,0 +1,12 @@
+package algoexpert.recursion
+
+fun productSum(splArr: List<Any>, depth: Int = 1): Int {
+  var sum = 0
+  for (i in splArr) {
+    sum += depth * when {
+      i is Int -> i
+      else -> productSum(i as List<Any>, depth + 1)
+    }
+  }
+  return sum
+}
---
+++ ds-algo/src/test/kotlin/algoexpert/recursion/ProductSumTest.kt
@@ -0,0 +1,20 @@
+package algoexpert.recursion
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class ProductSumTest :
+  StringSpec({
+   "product sum" {
+       forAll(
+         row(listOf(5, 2, listOf(7, -1), 3, listOf(6, listOf(-13, 8), 4)), 12),
+         row(listOf(1,2,3,4,5), 15),
+         row(listOf(1,2,listOf(3),4,5), 18),
+         row(listOf(listOf(listOf(listOf(listOf(5))))), 600),
+       ) { splArr: List<Any>, result ->
+          productSum(splArr) shouldBe result
+       }
+  }
+})
