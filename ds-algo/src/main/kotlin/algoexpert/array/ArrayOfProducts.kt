d8c367341d2e30a7490d8c8902e33553d0c3f262
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2023-07-26 13:35:47 +0530

    ArrayOfProducts

---
+++ ds-algo/src/main/kotlin/algoexpert/array/ArrayOfProducts.kt
@@ -0,0 +1,13 @@
+package algoexpert.array
+
+fun IntArray.productArray(): IntArray {
+  val leftToRightProduct = runningReduce { acc, item -> acc * item }
+  val rightToLeftProduct = reversed().runningReduce { acc, item -> acc * item }.reversed()
+  val result = IntArray(size)
+  result[0] = rightToLeftProduct[1]
+  result[result.lastIndex] = leftToRightProduct[leftToRightProduct.lastIndex - 1]
+  for (i in 1 until result.lastIndex) {
+    result[i] = leftToRightProduct[i - 1] * rightToLeftProduct [i + 1]
+  }
+  return result
+}
---
+++ ds-algo/src/test/kotlin/algoexpert/array/ArrayOfProductsTest.kt
@@ -0,0 +1,20 @@
+package algoexpert.array
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class ArrayOfProductsTest :
+  StringSpec({
+    "array of products" {
+      forAll(
+        row(intArrayOf(5, 1, 4, 2), intArrayOf(8, 40, 10, 20)),
+        row(intArrayOf(1,8,6,2,4), intArrayOf(384, 48, 64, 192, 96)),
+        row(intArrayOf(-5,2,-4,14,-6), intArrayOf(672, -1680, 840, -240, 560)),
+        row(intArrayOf(4, 4), intArrayOf(4, 4)),
+      ) { arr, resultArr ->
+         arr.productArray() shouldBe resultArr
+      }
+    }
+  })
