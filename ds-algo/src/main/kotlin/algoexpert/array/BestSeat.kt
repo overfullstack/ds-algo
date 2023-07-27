18447f4714541e82b2efb5f41902f97db69e0dfd
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2023-07-27 17:08:59 +0530

    Best Seat

---
+++ ds-algo/src/main/kotlin/algoexpert/array/BestSeat.kt
@@ -0,0 +1,22 @@
+package algoexpert.array
+
+fun bestSeat(seatArr: IntArray): Int {
+  var maxZeroCount = Int.MIN_VALUE
+  var maxZeroCountStartIndex: Int = -1
+  var zeroCountStartIndex: Int = -1
+  var i = 0
+  while (i <= seatArr.lastIndex) {
+    if (seatArr[i] == 0) {
+      zeroCountStartIndex = i
+      while (i <= seatArr.lastIndex && seatArr[i] == 0) {
+        i++
+      }
+      if (i - zeroCountStartIndex > maxZeroCount) {
+        maxZeroCountStartIndex = zeroCountStartIndex
+        maxZeroCount = i - zeroCountStartIndex
+      }
+    }
+    i++
+  }
+  return if (zeroCountStartIndex == -1) -1 else (maxZeroCountStartIndex + (maxZeroCount - 1)/ 2)
+}
---
+++ ds-algo/src/test/kotlin/algoexpert/array/BestSeatTest.kt
@@ -0,0 +1,24 @@
+package algoexpert.array
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class BestSeatTest :
+  StringSpec({
+    "best seat" {
+      forAll(
+        row(intArrayOf(1, 0, 1, 0, 0, 0, 1), 4),
+        row(intArrayOf(1), -1),
+        row(intArrayOf(1,0,1), 1),
+        row(intArrayOf(1,0,0,1), 1),
+        row(intArrayOf(1,1,1), -1),
+        row(intArrayOf(1,0,0,1,0,0,1), 1),
+        row(intArrayOf(1,0,0,0,0,0,0,1,0,0,1), 3),
+        row(intArrayOf(1,1,1,0,1,0,0,1,1,1,0,0,1), 5),
+      ) { seatArr, result ->
+         bestSeat(seatArr) shouldBe result
+      }
+    }
+  })
