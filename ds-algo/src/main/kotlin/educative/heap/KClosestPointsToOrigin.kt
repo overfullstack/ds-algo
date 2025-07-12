375005c24542e38a9d171a6c1734c3f76073839b
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-07-12 11:17:10 +0530

    KClosestPointsToOrigin

---
+++ ds-algo/src/main/kotlin/educative/heap/KClosestPointsToOrigin.kt
@@ -0,0 +1,20 @@
+package educative.heap
+
+import java.util.PriorityQueue
+import kotlin.math.sqrt
+import kotlin.math.pow
+
+fun kClosestPointsToOrigin(points: List<Pair<Int, Int>>, k: Int): List<Pair<Int, Int>> {
+  val maxHeap = PriorityQueue(Comparator.comparingDouble(::distanceFromOrigin))
+  points.take(k).forEach { maxHeap.add(it) }
+  points.drop(k).map {
+    if (distanceFromOrigin(it) < distanceFromOrigin(maxHeap.peek())) {
+      maxHeap.poll()
+      maxHeap.add(it)
+    }
+  }
+  return maxHeap.toList()
+}
+
+fun distanceFromOrigin(point: Pair<Int, Int>): Double = 
+  sqrt(point.first.toDouble().pow(2) + point.second.toDouble().pow(2))
