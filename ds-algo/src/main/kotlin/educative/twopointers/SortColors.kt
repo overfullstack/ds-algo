9e0045148a70d4a3b4c78baf137702384899df74
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-07-14 11:20:46 +0530

    SortColors

---
+++ ds-algo/src/main/kotlin/educative/twopointers/SortColors.kt
@@ -0,0 +1,33 @@
+package educative.twopointers
+
+
+/* 14 Jul 2025 10:41 */
+
+fun sortColors(arr: IntArray): IntArray {
+  var start = 0
+  var end = arr.lastIndex
+  var current = start
+  while (current < end) {
+    when (arr[current]) {
+      0 -> {
+        arr.swap(start, current)
+        start++
+        current++
+      }
+      1 -> current++
+      2 -> {
+        arr.swap(current, end)
+        end--
+      }
+    }
+  }
+  return arr
+}
+
+private fun IntArray.swap(index1: Int, index2: Int) {
+  if (index1 != index2 && this[index1] != this[index2]) {
+    var temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
+  }
+}
