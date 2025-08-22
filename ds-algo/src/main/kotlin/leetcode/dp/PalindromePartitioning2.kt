3db75f47ea9d0c8ca08cc7cab55d3334716b1a92
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-08-22 23:13:57 +0530

    ThreeSum
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

--- ds-algo/src/main/kotlin/leetcode/dp/PalindromePartitioning2.kt
+++ ds-algo/src/main/kotlin/leetcode/dp/PalindromePartitioning2.kt
@@ -2,29 +2,30 @@
 package leetcode.dp
 
 /** [132. Palindrome Partitioning II](https://leetcode.com/problems/palindrome-partitioning-ii/) */
-fun minCutsForPalindromePartition(s: String): Int {
+fun minCut(s: String): Int {
   val cutsTable = Array(s.length) { IntArray(s.length) }
   val isPalTable = Array(s.length) { BooleanArray(s.length) }
 
-  // ! For problems with `gap`, especially Palindrome related problems, always initiate table's
-  // diagonal.
+  // ! For problems with `windowLen`, especially Palindrome related problems, 
+  // always initiate table's diagonal.
   for (index in cutsTable.indices) {
     cutsTable[index][index] = 0 // No cuts required for single letter
     isPalTable[index][index] = true // Every single letter is a palindrome
   }
 
-  for (gap in 1..s.lastIndex) {
-    for ((i, j) in (gap..s.lastIndex).withIndex()) {
+  for (windowLen in 1..s.lastIndex) {
+    for ((i, j) in (windowLen..s.lastIndex).withIndex()) {
       isPalTable[i][j] =
         // ! This helps in not calculating cuts when it's already a palindrome
-        if (gap == 1) s[i] == s[j] else s[i] == s[j] && isPalTable[i + 1][j - 1]
+        if (windowLen == 1) s[i] == s[j] else (s[i] == s[j] && isPalTable[i + 1][j - 1])
       // This stores no.of cuts required for substring(i..j).
       // If all are distinct characters, it's equal to length of the substring.
       cutsTable[i][j] =
         if (isPalTable[i][j]) 0
-        // +1 is the extra cut required for partitioning two palindromes from `i-partition` and
-        // `partition-j`
+        // +1 is the extra cut required for partitioning two palindromes from `i..partition` and
+        // `partition+1..j`
         // If no substring is a palindrome, this +1 makes the result = no.of chars in the string.
+        // `j` is excluded as we do `it + 1` for partition
         else (i until j).minOfOrNull { cutsTable[i][it] + 1 + cutsTable[it + 1][j] } ?: 0
     }
   }
@@ -32,5 +33,5 @@ fun minCutsForPalindromePartition(s: String): Int {
 }
 
 fun main() {
-  print(minCutsForPalindromePartition(readln()))
+  print(minCut(readln()))
 }
