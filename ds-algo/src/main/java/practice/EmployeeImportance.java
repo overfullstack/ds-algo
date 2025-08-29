125d948c4ec678c717c96daf699bc63e5699c447
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-08-29 18:18:29 +0530

    EmployeeImportance
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

---
+++ ds-algo/src/main/java/practice/EmployeeImportance.java
@@ -0,0 +1,54 @@
+package practice;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * [690. Employee Importance](https://leetcode.com/problems/employee-importance/)
+ */
+public class EmployeeImportance {
+
+  public int getImportance(List<Employee> employees, int id) {
+    var employeeRegistry =
+        employees.stream().collect(Collectors.toMap(e -> e.id, Function.identity()));
+    return dfs(id, employeeRegistry);
+  }
+
+  private static int dfs(
+      Integer eId, Map<Integer, Employee> employeeRegistry) {
+    var employee = employeeRegistry.get(eId);
+    return employee.importance
+        + employee.subordinates.stream()
+        .mapToInt(seId -> dfs(seId, employeeRegistry))
+        .sum();
+  }
+
+  private static class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+
+    public Employee(int id, int importance, List<Integer> subordinates) {
+      this.id = id;
+      this.importance = importance;
+      this.subordinates = subordinates;
+    }
+  }
+
+  static void main() {
+    var employeeImportance = new EmployeeImportance();
+    var employees = List.of(
+        new Employee(1, 5, List.of(2, 3)),
+        new Employee(2, 3, List.of()),
+        new Employee(3, 3, List.of())
+    );
+    System.out.println(employeeImportance.getImportance(employees, 1));
+    var employees2 = List.of(
+        new Employee(1, 2, List.of(5)),
+        new Employee(5, -3, List.of())
+    );
+    System.out.println(employeeImportance.getImportance(employees2, 5));
+  }
+}
