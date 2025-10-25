dda0162b24777380eef5261e8e37fe534359cbec
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2025-10-25 14:44:58 +0530

    LetterTilePossibilities
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

---
+++ ds-algo/src/main/java/practice/subsets/LetterTilePossibilities.java
@@ -0,0 +1,32 @@
+package practice.subsets;
+
+/* 25 Oct 2025 14:35 */
+
+public class LetterTilePossibilities {
+  public int numTilePossibilities(String tiles) {
+    var freq = new int[26];
+    for (var ch: tiles.toCharArray()) {
+      freq[ch - 'A']++;
+    }
+    return permutationsAndCombinationsCount(freq);
+  }
+  
+  private static int permutationsAndCombinationsCount(int[] freq) {
+    var count = 0;
+    for (var i = 0; i < 26; i++) {
+      if (freq[i] > 0) {
+        freq[i]--;
+        count++;
+        count += permutationsAndCombinationsCount(freq);
+        freq[i]++;
+      }
+    }
+    return count;
+  }
+  
+  static void main() {
+    var l = new LetterTilePossibilities();
+    System.out.println(l.numTilePossibilities("AAB")); // 8
+    System.out.println(l.numTilePossibilities("AAABBC")); // 188
+  }
+}
