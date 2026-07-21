7091367e46d122666a29f5d7e3ce55cab252c3a5
Gopal S Akshintala <gopalakshintala@gmail.com>
2026-07-21 21:06:37 +0530

    refactor(testcase): migrate educative/greedy JumpGame to TestCases model

--- ds-algo/src/test/kotlin/educative/greedy/JumpGameTest.kt
+++ ds-algo/src/test/kotlin/educative/greedy/JumpGameTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 import leetcode.greedy.canJump
-import testcase.ListToBool.Companion.parseJsonFileToTestCases
+import testcase.TestCases
 
 /* 13 Jul 2025 11:41 */
 
@@ -13,7 +13,8 @@ private const val PKG_PATH = "educative/greedy/JumpGame"
 class JumpGameTest :
   StringSpec({
     "Jump Game" {
-      parseJsonFileToTestCases("$PKG_PATH/test-cases-1.json", "$PKG_PATH/test-cases-2.json")
+      TestCases.load("$PKG_PATH/test-cases-1.json", "$PKG_PATH/test-cases-2.json")
+        .map { case -> case.input<List<Int>>(1) to case.output<Boolean>(1) }
         .forAll { (input, output) -> canJump(input.toIntArray()) shouldBe output }
     }
   })
